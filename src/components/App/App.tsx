a6204adfd6de8f4adc59d4ca95fae09175125071
Mariana_k <m.koval000@gmail.com>
2025-07-03 13:53:11 +0300

    add

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-// import css from './App.module.css';
+import css from "./App.module.css";
 import { useState } from "react";
 import type { Movie } from "../../types/movie";
 import { toast, Toaster } from "react-hot-toast";
